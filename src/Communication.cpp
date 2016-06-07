bbdbb73ad5ac63a05ef6ca45ee47c1cacf900bc2
Tymoteusz Bleja <thymson@windowslive.com>
2016-06-07 18:01:56 +0200

    polluks test

--- src/Communication.cpp
+++ src/Communication.cpp
@@ -70,9 +70,12 @@ void Communication::run() {
       struct singleParticipantData myRequest;
       myRequest.id = this->mpiRank;
       myRequest.lamport = lamportCopy;
+      char* procname;
+      int* a;
 
       if (*this->status == 1) {
-
+        MPI_Get_processor_name(procname, a);
+        cout << procname << endl;
         // Wyślij żądanie do wszystkich procesów (sekcja OPEN), wstaw wszystkie IDki procesów do mojego awaitingAnswerList
         for (i = 0; i < this->mpiSize; i++) {
           if (i != this->mpiRank) {
